c858440e15b75a005f50fdebc7d923e47b8d5d3f
Vincent Gabriel <vadimg88@gmail.com>
2017-02-04 20:23:27 -0800

    Update WidgetServiceProvider.php

--- WidgetServiceProvider.php
+++ WidgetServiceProvider.php
@@ -19,10 +19,10 @@ class WidgetServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app['widget'] = $this->app->share(function ($app) {
-            $blade = $app['view']->getEngineResolver()->resolve('blade')->getCompiler();
+        $this->app->singleton('widget', function ($app) {
+          $blade = $app['view']->getEngineResolver()->resolve('blade')->getCompiler();
 
-            return new Widget($blade, $app);
+          return new Widget($blade, $app);
         });
 
         $this->app->booting(function () {
